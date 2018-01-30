3c5fb2c446b7e92a6b143c0b3e630aa5beb00ab9
Tommy Howell <tommyhowell6@gmail.com>
2018-01-29 23:40:51 -0700

    connected game fragment to with xml

--- app/src/main/java/com/dominion/Fragments/GameFragment.java
+++ app/src/main/java/com/dominion/Fragments/GameFragment.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.dominion.R;
 
@@ -15,6 +17,7 @@ import com.dominion.R;
 
 public class GameFragment extends Fragment
 {
+    private Button mStartGameButton;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState)
     {
@@ -23,9 +26,20 @@ public class GameFragment extends Fragment
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState)
+    public View onCreateView(LayoutInflater inflater, @Nullable final ViewGroup container, Bundle savedInstanceState)
     {
         final View v = inflater.inflate(R.layout.game_fragment, container, false);
+
+        mStartGameButton = (Button) v.findViewById(R.id.start_game_button);
+        mStartGameButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(container.getContext(), "Start Game", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return v;
     }
 }
