10d587aa00b5fe06c74a2850a37351a410d03982
Tommy Howell <tommyhowell6@gmail.com>
2018-02-11 00:29:30 -0700

    added shield.png

---
+++ app/src/main/java/com/dominion/Views/CardView.java
@@ -0,0 +1,17 @@
+package com.dominion.Views;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by tommyhowell on 2/10/18.
+ */
+
+public class CardView extends View
+{
+    public CardView(Context context)
+    {
+        super(context);
+    }
+    
+}
