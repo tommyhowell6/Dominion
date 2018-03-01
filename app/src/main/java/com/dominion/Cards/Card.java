4452311ead44cee411c69f07d2f7271fb7cc8381
Tommy Howell <tommyhowell6@gmail.com>
2018-02-28 19:19:58 -0700

    working on getting card and deck to implement parcelable

--- app/src/main/java/com/dominion/Cards/Card.java
+++ app/src/main/java/com/dominion/Cards/Card.java
@@ -1,13 +1,15 @@
 package com.dominion.Cards;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.dominion.Player;
 
 /**
  * Created by tommyhowell on 1/28/18.
  */
 
-public class Card
-{
+public class Card implements Parcelable {
     private String name;
     private int cost;
 
@@ -26,4 +28,33 @@ public class Card
     {
         return cost;
     }
+
+    protected Card(Parcel in) {
+        name = in.readString();
+        cost = in.readInt();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeInt(cost);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Card> CREATOR = new Parcelable.Creator<Card>() {
+        @Override
+        public Card createFromParcel(Parcel in) {
+            return new Card(in);
+        }
+
+        @Override
+        public Card[] newArray(int size) {
+            return new Card[size];
+        }
+    };
 }
--- app/src/main/java/com/dominion/Decks/Deck.java
+++ app/src/main/java/com/dominion/Decks/Deck.java
@@ -1,5 +1,8 @@
 package com.dominion.Decks;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.dominion.Cards.Card;
 
 import java.util.ArrayList;
@@ -8,7 +11,48 @@ import java.util.ArrayList;
  * Created by tommyhowell on 1/28/18.
  */
 
-public class Deck
+public class Deck implements Parcelable
 {
+    public Deck()
+    {
+    }
+
     ArrayList<Card> cards;
+
+    protected Deck(Parcel in) {
+        if (in.readByte() == 0x01) {
+            cards = new ArrayList<Card>();
+            in.readList(cards, Card.class.getClassLoader());
+        } else {
+            cards = null;
+        }
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        if (cards == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeList(cards);
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Deck> CREATOR = new Parcelable.Creator<Deck>() {
+        @Override
+        public Deck createFromParcel(Parcel in) {
+            return new Deck(in);
+        }
+
+        @Override
+        public Deck[] newArray(int size) {
+            return new Deck[size];
+        }
+    };
 }
